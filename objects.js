c9407ffc9ee147c8df95a189f43efd6864417d60
Timothy Rines <tsrines@gmail.com>
2019-08-12 15:40:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -17,11 +17,11 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
   
   return playlist
+}
 
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName]
-}
-return playlist;
+  return playlist;
 }
   
 
