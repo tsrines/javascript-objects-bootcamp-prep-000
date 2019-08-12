eb14f2278e064fea7255dfa86a4e6d445d1ca596
Timothy Rines <tsrines@gmail.com>
2019-08-12 15:20:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,28 +2,10 @@
 
 
 
-var playlist = {                              //
-  "Slowdive": "Alison",
-  "My Bloody Valentine" : "Sometimes"
-};
-
-var artistName = "Phil Ochs"
-var songTitle = "Here\'s to the State of Mississippi"
-
-function updatePlaylist(playlist , artistName , songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
-}
-
-function removeFromPlaylist(playlist , artistName){
-  delete playlist[artistName]
-  console.log(playlist)
-  return playlist
-}
-
-
+var playlist = {
   
-
+  
+}
 
 
 
