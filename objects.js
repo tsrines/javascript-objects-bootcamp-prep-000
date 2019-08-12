83828bdc0bdd38df44469e3bd4da24ad5c54363c
Timothy Rines <tsrines@gmail.com>
2019-08-12 15:36:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,13 +3,24 @@
 
 
 var playlist = {
+  "Slowdive": "Alison"
+  "My Bloody Valentine": "Sometimes"
+
+
+
+
+var artistName = "Phil Ochs"
+var songTitle = "Here\'s to the State of Mississippi"
+
+function updatePlaylist(playlist, artistName, songTitle){
   
+  playlist[artistName] = songTitle
+  
+  return playlist
+}
   
 }
-
-
-
-
+  
 
 
 
